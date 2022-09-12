bb0ed1de01ec3b268fa8f70afc696941b6db9a03
Fabian Zelaya <fabianzelaya@icloud.com>
2022-09-11 18:45:17 -0700

    ListDemo

---
+++ ListDemo/ListDemo.java
@@ -0,0 +1,48 @@
+package ListDemo;
+
+/**
+ * @author Fabian Zelaya
+ * @version 1.0
+ * @created Sun Sep 11, 20222
+ * 
+ * @description ListDemo.java
+ * 
+ * @instructions Here are the LinkedList and ListIterator classes and a program
+ *               that demonstrates linked list operations.
+ * 
+ */
+
+/**
+ * A program that demonstrates the LinkedList class
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        LinkedList staff = new LinkedList();
+        staff.addFirst("Tom");
+        staff.addFirst("Romeo");
+        staff.addFirst("Harry");
+        staff.addFirst("Diana");
+
+        // | in the comments indicates the iterator position
+        ListIterator iterator = staff.listIterator(); // |DHRT
+        iterator.next(); // D|HRT
+        iterator.next(); // DH|RT
+
+        // Add more elements after second element
+        iterator.add("Juliet"); // DHJ|RT
+        iterator.add("Nina"); // DHJN|RT
+
+        iterator.next(); // DHJNR|T
+
+        // Remove last traversed element
+        iterator.remove(); // DHJN|T
+
+        // Print all elements
+        iterator = staff.listIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+}
+// FZ
